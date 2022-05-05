5623a185b4b372841a9cd48e91768b551f1c6d3b
Inuxoid <alpha.inuxoid@gmail.com>
2022-05-05 21:56:42 +0300

    Insertion sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] A = new int[10];
+Random rand = new Random();
+
+for (int i = 0; i < A.Length; i++)
+    A[i] = rand.Next(1, 10);
+
+Console.WriteLine("[{0}]", string.Join(", ", A));
+
+InsertionSort(A);
+
+Console.WriteLine("[{0}]", string.Join(", ", A));
+
+int[] InsertionSort(int[] A)
+{
+    int i;
+    int j;
+    int key;
+
+    for (j = 1; j < A.Length; j++)
+    {
+        key = A[j];
+        i = j - 1;
+        while (i >= 0 && A[i] > key)
+        {
+            A[i + 1] = A[i];
+            i--;
+        }
+        A[i+ 1] = key;
+    }
+
+    return A;
+}
